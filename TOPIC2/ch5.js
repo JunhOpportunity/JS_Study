87ab3907f50ea923f8d56eb092d72abacfe2aedb
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-10-23 23:53:29 +0900

    JS filter & find Systex Practice

--- TOPIC2/ch5.js
+++ TOPIC2/ch5.js
@@ -16,3 +16,26 @@ firstNames.forEach((firstName, index) => {
 // forEach 와의 차이점은, map 에서는 값을 반환 해 준다는 것이다.
 // 따라서, 반복문 앞에 변수를 지정해 주면, 그 변수에 값을 리턴해 줄 수 있다.
 // forEach 구문에서는 그게 불가능하다.
+
+
+
+// filter
+// filter을 이용해 새로운 배열을 만들 수 있음. -> 하나만 필터링해도 배열로 출력됨. 이래서 해결 방안 = find
+// 객체들을 묶은 배열 만들었음.
+
+const devices = [
+    {name: 'S20', brand: 'samsung'},
+    {name: 'mac', brand: 'apple'},
+];
+
+// 요소의 프로퍼티가 apple 인 것만 리턴
+const apples_filter = devices.filter((el) => el.brand === 'apple');
+
+console.log(apples_filter);
+
+// find
+// filter 은 배열로 리턴하지만, find 는 값으로 리턴함.
+// 그런데, find 는 하나만 찾고 바로 종료됨.
+const apples_find = devices.find((el) => el.brand === 'apple');
+
+console.log(apples_find);
